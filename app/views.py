3683e08dbffdff6cb15788875a9f791dc16593bc
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-11 23:59:10 +0300

    created the register view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
-    return render(request,'index.html')
+    return render(request,'welcome.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST) 
+        if form.is_valid():
+            form.save() # Save user to Database
+            username = form.cleaned_data.get('username') # Get the username that is submitted
+            messages.success(request, f'Account created for {username}!') # Show sucess message when account is created
+            return redirect('login') # Redirect user to Homepage
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/registration.html', {'form': form})
+
+@login_required # Require user logged in before they can access profile page
+def profile(request):
+    return render(request, 'users/profile.html')
