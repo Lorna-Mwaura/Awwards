4bc0a9b57df448ea5a8f47efd746ff47e1ce4c70
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-13 00:17:50 +0300

    created the project models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+from django.urls import reverse
+import datetime as dt
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
@@ -9,4 +14,26 @@ class Profile(models.Model):
     bio = models.TextField(max_length=100, blank=True)
     contact = models.TextField(max_length=100, blank=True)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
     
+    def save(self, **kwargs):
+        super().save()
+    
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user = id).first()
+        return details
+
+class Project(models.Model):
+    image = CloudinaryField('images')
+    title = models.CharField(max_length =100)
+    url = models.CharField(max_length =80)
+    detail_desciption=models.TextField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+   
