096cf267da0ece780be2b2a3655875e68122f565
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-13 23:55:54 +0300

    added the uploadform

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Project
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +10,9 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class uploadform(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ['image', 'title', 'detail_desciption','url']
